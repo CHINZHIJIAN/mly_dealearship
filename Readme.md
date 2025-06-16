894cab56342c8b74659a1f8d0089da9d77867e4a
CHINZHIJIAN <m-10305730@moe-dl.edu.my>
2025-06-16 15:49:29 +0800

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Netic - Free Bootstrap Web Hosting Website Template
+
+#### Preview
+
+ - [Demo](https://themewagon.github.io/Malaysia Dealership/)
+
+#### Download
+ - [Download from ThemeWagon](https://themewagon.com/themes/Malaysia Dealership/)
+ 
+ 
+## Getting Started
+
+Clone from Github 
+```
+git clone https://github.com/themewagon/Malaysia Dealership.git
+```
+
+## Author
+
+Design and code is completely written by HTML Design's design and development team.  
+
+
+## License
+
+ - Design and Code is Copyright &copy; [HTML Design](https://html.design/)
+ - Licensed cover under [MIT]
+ - Distributed by [ThemeWagon](https://themewagon.com)
+
---
+++ app.js
@@ -0,0 +1,121 @@
+{
+  "site": {
+    "title": "Malaysia Dealership",
+    "logo": "Malaysia Dealership",
+    "meta": {
+      "keywords": "",
+      "description": "",
+      "author": ""
+    },
+    "navigation": [
+      {"name": "Home", "url": ""},
+      {"name": "Models", "url": ""},
+      {"name": "About", "url": ""},
+      {"name": "Price", "url": ""},
+      {"name": "Blog", "url": ""},
+      {"name": "Contact", "url": ""}
+    ]
+  },
+  "banner": {
+    "slides": [
+      {
+        "title": "New Model Cars",
+        "description": "MALAYSIA DEALERSHIP 2025",
+        "searchPlaceholder": "Search here"
+      },
+      // 2 more identical slides
+    ]
+  },
+  "about": {
+    "title": "About Shoroom",
+    "description": "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+    "image": "about-img.png"
+  },
+  "models": [
+    {
+      "id": 1,
+      "name": "New Carolo car",
+      "description": "Ullamco laboris nisi ut aliquip ex ea commodo consequat...",
+      "price": "$30000.00",
+      "image": "img-1.png"
+    },
+    {
+      "id": 2,
+      "name": "New Carolo car",
+      "description": "Ullamco laboris nisi ut aliquip ex ea commodo consequat...",
+      "price": "$30000.00",
+      "image": "img-2.png"
+    },
+    {
+      "id": 3,
+      "name": "New Carolo car",
+      "description": "Ullamco laboris nisi ut aliquip ex ea commodo consequat...",
+      "price": "$30000.00",
+      "image": "img-3.png"
+    }
+  ],
+  "stats": [
+    {
+      "icon": "icon-1.png",
+      "value": "3700",
+      "label": "Happy Clients"
+    },
+    {
+      "icon": "icon-2.png",
+      "value": "5700",
+      "label": "Vehicles In Stock"
+    },
+    {
+      "icon": "icon-3.png",
+      "value": "124",
+      "label": "Awards"
+    },
+    {
+      "icon": "icon-4.png",
+      "value": "704",
+      "label": "Dealer Branches"
+    }
+  ],
+  "featuredVehicles": [
+    {
+      "name": "Voluptate",
+      "description": "Commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+      "price": "$40000.0",
+      "image": "img-4.png"
+    },
+    // 2 more similar vehicles
+  ],
+  "testimonials": {
+    "title": "What Says Our Students",
+    "subtitle": "It is a long established fact that a reader will be distracted by the readable c",
+    "items": [
+      {
+        "name": "DenoMark",
+        "text": "anything embarrassing hidden in the middle of text...",
+        "image": "client-img.png"
+      }
+    ]
+  },
+  "contact": {
+    "title": "Get In Touch",
+    "formFields": [
+      {"type": "text", "placeholder": "Name", "name": "Name"},
+      {"type": "text", "placeholder": "Phone Number", "name": "Phone Number"},
+      {"type": "text", "placeholder": "Email", "name": "Email"},
+      {"type": "textarea", "placeholder": "Massage", "name": "Massage"}
+    ],
+    "contactInfo": {
+      "phone": "(+71) 8522369417",
+      "email": "demo@gmail.com"
+    },
+    "socialMedia": [
+      {"platform": "facebook", "icon": "fa-facebook"},
+      {"platform": "twitter", "icon": "fa-twitter"},
+      {"platform": "linkedin", "icon": "fa-linkedin"},
+      {"platform": "instagram", "icon": "fa-instagram"}
+    ]
+  },
+  "footer": {
+    "copyright": "2020 All Rights Reserved. Design by Free Html Templates. Distributed by ThemeWagon"
+  }
+}
